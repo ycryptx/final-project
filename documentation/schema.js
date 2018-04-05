a0fe16161aff474110d8ca86c90527759cbf402a
ycryptx <yonatan.med@gmail.com>
2018-04-04 20:33:18 -0400

    Add files via upload

---
+++ documentation/schema.js
@@ -0,0 +1,16 @@
+//schema 1
+
+const facilities = {facn: String,
+                    frsid : Number,
+                    fname : String,
+                    address: String,
+                     city: String,
+                      state: String,
+                       zip: Number,
+                       fips: Number
+                       county: String,
+                      lat: Number,
+                       lon: Number
+                     }
+
+// schma 2

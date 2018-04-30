a42cd8e8ea01700b67215e9fb10a05914aaa4e61
ycryptx <yonatan.med@gmail.com>
2018-04-30 13:42:26 -0400

    change error log

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use('/', indexRouter);
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+  console.log('Error!', err);
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
